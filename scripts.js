8f1924a33a5f01d42fef94df6868277e3b7f960e
Alex Szeliga <alex@deliverycircle.com>
2021-08-05 20:01:25 -0400

    part way through 3

--- scripts.js
+++ scripts.js
@@ -1,28 +1,34 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
 window.onload = (event) => {
+    const takeoffButton = document.getElementById('takeoff');
+
     const status = document.getElementById('flightStatus');
     const bg = document.getElementById('shuttleBackground');
     const height = document.getElementById('spaceShuttleHeight');
-    const takeoffButton = document.getElementById('takeoff');
+
     const landButton = document.getElementById('landing');
 
-    takeoffButton.onclick = (e)=>takeOffClick(e,status,height, bg);
-    landButton.onclick = landClick;
-}
-
-function takeOffClick(e,status, height,bg) {
-    const userInput = window.confirm(
-        "Confirm that the shuttle is ready for takeoff."
-    );
-    if (userInput) {
-        let newHeight = parseInt(height.textContent) + 10000;
-        status.textContent = "Shuttle in flight.";
-        height.textContent = String(newHeight);
-        bg.style.backgroundColor = "blue";
+    takeoffButton.onclick = takeOffClick;
+    landButton.onclick = function(e){
+        landClick(e, bg);
+    };
+    function takeOffClick(e) {
+        const userInput = window.confirm(
+            "Confirm that the shuttle is ready for takeoff."
+        );
+        if (userInput) {
+            let newHeight = parseInt(height.textContent) + 10000;
+            status.textContent = "Shuttle in flight.";
+            height.textContent = String(newHeight);
+            bg.style.backgroundColor = "blue";
+        }
     }
 }
 
-function landClick(event) {
 
+
+function landClick(event, bg) {
+    window.alert("We're landing or w/e");
+    bg.style.backgroundColor = "green";
 }
